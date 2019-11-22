f3c9228f87bee5a8b92dbb95d2555aadfab26781
Lela Smith <smith.lelaa@gmail.com>
2019-11-22 11:14:27 -0800

    add html and separate logic.

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,7 @@
 export class BetterDoctorApi {
-  async getApiData(search){
-    try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=28dc6ef66c985a5b07ae1edcf3ca42e6`);
-      let jsonifiedResponse = await response.json();
-      return jsonifiedResponse;
-    } catch(error) {
-      console.error("There was an error handling your request: " + error.message);
-    }
+  async getApiData() {
+    let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=john&user_key=28dc6ef66c985a5b07ae1edcf3ca42e6`);
+    let jsonifiedResponse = await response.json();
+    return jsonifiedResponse;
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,3 +2,23 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { BetterDoctorApi } from './../src/apiCalls.js';
+
+
+$(document).ready(function() {
+  $('#doc-search').submit(function(event) {
+    event.preventDefault();
+
+
+    (async () => {
+      let betterDoctorApi = new BetterDoctorApi();
+      let response = await betterDoctorApi.getApiData();
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      $('.doctor-info').text(response);
+    }
+
+  });
+});

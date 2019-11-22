3a70c65790d771f8661840de52120be5d27f52aa
Lela Smith <smith.lelaa@gmail.com>
2019-11-22 15:33:23 -0800

    add input that takes in medical concern

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,6 @@
 export class BetterDoctorApi {
-  async getApiData() {
-    let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+  async getApiData(concern) {
+    let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${concern}location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
     let jsonifiedResponse = await response.json();
     return jsonifiedResponse;
   }
--- src/main.js
+++ src/main.js
@@ -9,14 +9,17 @@ $(document).ready(function() {
   $('#doc-search').submit(function(event) {
     event.preventDefault();
 
+     let concern= $("input#concern").val();
+
     (async () => {
       let betterDoctorApi = new BetterDoctorApi();
-      let response = await betterDoctorApi.getApiData();
+      let response = await betterDoctorApi.getApiData(concern);
       getElements(response);
     })();
 
     function getElements(response) {
       $('.showData').text(`${response.data[0].profile.first_name}` + " " + `${response.data[0].profile.last_name}` + "," + `${response.data[0].profile.title}`);
     }
+     document.getElementById("doc-search").reset();
   });
 });

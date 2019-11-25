ae3d7b8e61e1322077d516ee951fd5e53c9d1d90
Lela Smith <smith.lelaa@gmail.com>
2019-11-24 20:54:59 -0800

    add name input to search by name

--- src/main.js
+++ src/main.js
@@ -10,20 +10,25 @@ $(document).ready(function() {
     event.preventDefault();
 
     let concern= $("input#concern").val();
+    let name= ("input#name").val();
 
     (async () => {
-      let betterDoctorApi = new BetterDoctorApi(concern);
-      let response = await betterDoctorApi.getApiData(concern);
+      let betterDoctorApi = new BetterDoctorApi(concern, name);
+      let response = await betterDoctorApi.getApiData(concern, name);
       getElements(response);
     })();
-
     function getElements(response) {
-      $('.showData').text("")
+      $('.showData').text("");
       if (response.data.length === 0){
         $('.showData').text("There are no doctors in the Portland area that treat that issue.");
       } else {
         for (let i = 0; i < response.data.length; i++){
-          $('.showData').append(`${response.data[i].profile.first_name} ${response.data[i].profile.last_name},  ${response.data[i].profile.title} <br>  ${response.data[i].practices[0].name} <br> ${response.data[i].practices[0].visit_address.street} ${response.data[i].practices[0].visit_address.city}, ${response.data[i].practices[0].visit_address.state} <br>  Phone: ${response.data[i].practices[0].phones[0].number} <br> Accepting new patients: ${response.data[i].practices[0].accepts_new_patients} <br> <br>`);
+          $('.showData').append(`<br>${response.data[i].profile.first_name} ${response.data[i].profile.last_name},  ${response.data[i].profile.title} <br>  ${response.data[i].practices[0].name} <br> ${response.data[i].practices[0].visit_address.street} ${response.data[i].practices[0].visit_address.city}, ${response.data[i].practices[0].visit_address.state} <br>  Phone: ${response.data[i].practices[0].phones[0].number}<br>`);
+          if(response.data[i].practices[0].accepts_new_patients === true) {
+            $('.showData').append(`<strong>Accepting New Patients</strong><br><br>`);
+          } else {
+            $('.showData').append(`<strong>Not Accepting New Patients</strong><br>`);
+          }
         }
       }
     }

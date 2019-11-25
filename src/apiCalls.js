5356a7b265b4a1edd8746d636e37ea496f55bf07
Lela Smith <smith.lelaa@gmail.com>
2019-11-24 21:34:39 -0800

    add website to search results

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,9 @@
 export class BetterDoctorApi {
   async getApiData(concern, name) {
-    let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${concern}&query=${name}location=or-portland&user_location=45.505%2C-122.675&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+    let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${concern}&name=${name}&location=or-portland&user_location=45.505%2C-122.675&skip=0&limit=10&user_key=${process.env.API_KEY}`);
     let jsonifiedResponse = await response.json();
     return jsonifiedResponse;
   } catch(error) {
-      console.error("There was an error handling your request: " + error.message);
-    }
+    console.error("There was an error handling your request: " + error.message);
+  }
 }
--- src/main.js
+++ src/main.js
@@ -10,20 +10,21 @@ $(document).ready(function() {
     event.preventDefault();
 
     let concern= $("input#concern").val();
-    let name= ("input#name").val();
+    let name= $("input#name").val();
 
     (async () => {
       let betterDoctorApi = new BetterDoctorApi(concern, name);
       let response = await betterDoctorApi.getApiData(concern, name);
       getElements(response);
     })();
+
     function getElements(response) {
       $('.showData').text("");
       if (response.data.length === 0){
         $('.showData').text("There are no doctors in the Portland area that treat that issue.");
       } else {
         for (let i = 0; i < response.data.length; i++){
-          $('.showData').append(`<br>${response.data[i].profile.first_name} ${response.data[i].profile.last_name},  ${response.data[i].profile.title} <br>  ${response.data[i].practices[0].name} <br> ${response.data[i].practices[0].visit_address.street} ${response.data[i].practices[0].visit_address.city}, ${response.data[i].practices[0].visit_address.state} <br>  Phone: ${response.data[i].practices[0].phones[0].number}<br>`);
+          $('.showData').append(`<br>${response.data[i].profile.first_name} ${response.data[i].profile.last_name},  ${response.data[i].profile.title} <br>  ${response.data[i].practices[0].name} <br> ${response.data[i].practices[0].visit_address.street} ${response.data[i].practices[0].visit_address.city}, ${response.data[i].practices[0].visit_address.state} <br>Phone: ${response.data[i].practices[0].phones[0].number}<br> Website: <a href="${response.data[i].practices[0].website}">Click Here</a>"<br>`);
           if(response.data[i].practices[0].accepts_new_patients === true) {
             $('.showData').append(`<strong>Accepting New Patients</strong><br><br>`);
           } else {

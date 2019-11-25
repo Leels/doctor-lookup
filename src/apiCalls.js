be65af51991e3758f8d5d13140aeef8301cc75f4
Lela Smith <smith.lelaa@gmail.com>
2019-11-25 14:38:11 -0800

    Add styling

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,19 +1,23 @@
 export class BetterDoctorApi {
   async getApiData(concern, name) {
-    let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${concern}&name=${name}&location=or-portland&user_location=45.505%2C-122.675&skip=0&limit=10&user_key=${process.env.API_KEY}`);
-    let jsonifiedResponse = await response.json();
-    return jsonifiedResponse;
-  } catch(error) {
-    console.error("There was an error handling your request: " + error.message);
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${concern}&name=${name}&location=or-portland&user_location=45.505%2C-122.675&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch(error) {
+      console.error("There was an error handling your request: " + error.message);
+    }
   }
 }
 
 export class GiphyApi {
   async getGiphyApiData() {
-    let response2 = await fetch(`https://api.giphy.com/v1/gifs/A9MftKr3J3lra?api_key=${process.env.OTHER_API_KEY}`);
-    let jsonifiedResponse2 = await response2.json();
-    return jsonifiedResponse2;
-  } catch(error) {
-    console.error("There was an error handling your request: " + error.message);
+    try {
+      let response2 = await fetch(`https://api.giphy.com/v1/gifs/A9MftKr3J3lra?api_key=${process.env.OTHER_API_KEY}`);
+      let jsonifiedResponse2 = await response2.json();
+      return jsonifiedResponse2;
+    } catch(error) {
+      console.error("There was an error handling your request: " + error.message);
+    }
   }
 }
